b95960d5ebb4e78ba4bb4093956f12df740d81df
eyal-h <logme3@gmail.com>
2021-09-07 10:26:28 +0300

    tf files

--- provider.tf
+++ provider.tf
@@ -1,6 +1,14 @@
+terraform {
+  backend "remote" {
+    organization = "nimble-company-assignment"
+
+    workspaces {
+      name = "nimble-terraform-workspace"
+    }
+  }
+}
+
 provider "aws" {
-  access_key = "aa" //no-commit
-  secret_key = "aa" //no-commit
   region     = var.aws_region
   #if you are running from AWS ec2 linux instance please use bellow credentials section
   #shared_credentials_file = "$HOME/.aws/credentials"
